be660f424cea19ec274c7d913ea564941d22908e
dda <durov.da@gmail.com>
2024-11-13 22:47:40 +0300

    Добавил тест содержащий и некорректные данные тоже

--- solution_01/test_data_processor.py
+++ solution_01/test_data_processor.py
@@ -17,3 +17,29 @@ def test_calculate_results_when_data_is_correct():
     actual_result = processor._calculate_result(file_as_text_lines)
 
     assert actual_result == expected_result
+
+
+def test_calculate_results():
+    file_as_text_lines = [
+        'apple:  10',
+        'banana: 5',
+        'orange: три',
+        'grape:  два',
+        '',
+        'apple:  3',
+        'banana: 2',
+        'box of oranges',
+        'grape:  6'
+    ]
+
+    expected_result = {
+        'apple': 13,
+        'banana': 7,
+        'grape': 6,
+    }
+
+    processor = DataProcessor()
+    actual_result = processor._calculate_result(file_as_text_lines)
+
+    assert actual_result == expected_result
+
