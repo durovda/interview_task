ad5cd49a121a04b308c92977a17c71e4b5c05649
dda <durov.da@gmail.com>
2024-11-13 22:12:12 +0300

    Встроил в process_file() вызов _calculate_result()
    В process_file() заменил блок кода вызовом метода _calculate_result(). Теперь можно будет спокойно дорабатывать этот код, чтобы он мог работать и с некорректными дынными. А также можно спокойно рефакторить ("чистить") этот код, так как он уже находится под тестами (пока под одним)

--- solution_01/interview_task.py
+++ solution_01/interview_task.py
@@ -12,24 +12,9 @@ grape: 6
 class DataProcessor:
 
     def process_file(self, file_name):
-        res = {}
         file = open(file_name)
         f = [i for i in file]
-        for i in range(len(f)):
-            s = f[i].strip()
-            if type(s) == str:
-                s_list = s.split(':')
-                x = []
-                x.append(s_list[0])
-                x.append(s_list[1])
-                key = x[0]
-                value = int(x[1])
-                if key in res.keys():
-                    for exist_key, exist_value in res.items():
-                        if key == exist_key:
-                            res[key] = exist_value + value
-                else:
-                    res.update({key: value})
+        res = self._calculate_result(f)
         print("Итог по всем покупкам:")
         print(res)
 
