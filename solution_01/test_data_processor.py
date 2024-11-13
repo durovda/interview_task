ede7b09e7eea0faf3e560ecd5ef9cfd56ea3c4b5
dda <durov.da@gmail.com>
2024-11-13 23:12:48 +0300

    Собираюсь выделить метод _add_data_to_result()

--- solution_01/test_data_processor.py
+++ solution_01/test_data_processor.py
@@ -59,3 +59,17 @@ def test_convert_text_line_to_key_value_pair():
     processor = DataProcessor()
     key, value = processor._convert_text_line_to_key_value_pair('apple: 10')
     assert key == 'apple' and value == 10
+
+
+def test_add_data_to_result_when_add_item():
+    processor = DataProcessor()
+    result = {'apple': 13}
+    processor._add_data_to_result(key='banana', value=5, result=result)
+    assert result == {'apple': 13, 'banana': 5}
+
+
+def test_add_data_to_result_when_update_item():
+    processor = DataProcessor()
+    result = {'apple': 10}
+    processor._add_data_to_result('apple', 3, result)
+    assert result == {'apple': 13}
